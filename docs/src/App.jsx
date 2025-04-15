a1dc089b5b75c7d9d693593559336cf10c30ed7a
João Vitor <167632981+vitorjoaodev@users.noreply.github.com>
2025-04-15 18:45:04 -0300

    Delete docs directory

--- docs/src/App.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import './App.css';
-import AviationCourseDashboard from './AviationCourseDashboard';
-
-export default function App() {
-  return (
-    <AviationCourseDashboard />
-  );
-}
--- docs/src/index.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./App.css"; // Make sure this matches the exact case of your file
-import AviationCourseDashboard from "./AviationCourseDashboard";							
-ReactDOM.createRoot(document.getElementById('root')).render(
-	<React.StrictMode>
-		<App />
-	</React.StrictMode>
-)
